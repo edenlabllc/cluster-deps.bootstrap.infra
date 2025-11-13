eb0bc0eb7b5d2954d8e157f4e21881358ba47cea
Anton Novikov <a.novikov@edenlab.com.ua>
2025-11-13 18:00:08 +0100

    Rename postuninstall-preuninstall-XXX.sh hooks to follow alphabetic order.

--- bin/hooks/preuninstall-postuninstall-delete-gcp-cluster.sh
+++ bin/hooks/preuninstall-postuninstall-delete-gcp-cluster.sh
@@ -6,7 +6,7 @@ NAMESPACE="${1}"
 RELEASE_NAME="${2}"
 WAIT_FOR_CLUSTER_DELETION="${3:-false}"
 
-kubectl --namespace "${NAMESPACE}" delete cluster "${RELEASE_NAME}" --wait="${WAIT_FOR_CLUSTER_DELETION}"
+kubectl --namespace "${NAMESPACE}" delete cluster --selector "app.kubernetes.io/instance=${RELEASE_NAME}" --wait="${WAIT_FOR_CLUSTER_DELETION}"
 
 # Required for correct release removal of gcp-cluster via Helm
 sleep 3
